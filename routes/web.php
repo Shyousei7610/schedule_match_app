2cab735fef3c8cba3d1a95bee3ac75228522bd3b
yamamoto <b1949470@planet.kanazawa-it.ac.jp>
2022-01-27 21:28:17 +0900

    ユーザー登録＆メール認証を作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +20,40 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home');
+})->middleware('verified')
+  ->name('home');
 
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+
+Route::post('/signup', [App\Http\Controllers\SignupController::class, 'store']);
+
+Route::get('/login', function () {
+    return view('login');
+})->middleware('guest')
+  ->name('login');
+
+Route::post('/login', [App\Http\Controllers\LoginController::class, 'authenticate']);
+
+Route::get('/email/verify', function () {
+    return view('verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+Route::get('/logout', [App\Http\Controllers\LoginController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
